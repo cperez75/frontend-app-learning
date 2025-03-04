7e8f87ad54b74ce67a2855b175f7578b8859a885
Cristina Pérez <cperez@opensistemas.com>
2025-03-04 18:08:19 +0100

    Add node_modules

---
+++ node_modules/@edx/frontend-lib-learning-assistant/dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Xpert", {
+  enumerable: true,
+  get: function () {
+    return _Xpert.default;
+  }
+});
+Object.defineProperty(exports, "reducer", {
+  enumerable: true,
+  get: function () {
+    return _slice.reducer;
+  }
+});
+var _Xpert = _interopRequireDefault(require("./widgets/Xpert"));
+var _slice = require("./data/slice");
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+//# sourceMappingURL=index.js.map
